cbc2387cf935320d6d99fe8dad5f9212184a00eb
mirzokhid10 <mxudoyberdiyev21@gmail.com>
2024-01-21 14:22:27 +0500

    226. Category- Show data in index page

--- app/DataTables/CategoryDataTable.php
+++ app/DataTables/CategoryDataTable.php
@@ -22,7 +22,32 @@ class CategoryDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'category.action')
+            ->addColumn('action', function($query) {
+                $editBtn = "<a href='".route('admin.slider.edit', $query->id)."'
+                class='btn btn-primary'><i class='far fa-edit'></i></a>";
+                $deleteBtn = "<a href='".route('admin.slider.destroy', $query->id)."'
+                class='btn btn-danger ml-2 delete-item'><i class='far fa-trash-alt'></i></a>";
+
+                return $editBtn.$deleteBtn;
+            })
+            ->addColumn('icon', function($query){
+                return '<i style="font-size:30px" class="'.$query->icon.'"></i>';
+            })
+            ->addColumn('status', function($query){
+                if($query->status == 1){
+                    $button = '<label class="custom-switch mt-2">
+                        <input type="checkbox" checked name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status" >
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }else {
+                    $button = '<label class="custom-switch mt-2">
+                        <input type="checkbox" name="custom-switch-checkbox" data-id="'.$query->id.'" class="custom-switch-input change-status">
+                        <span class="custom-switch-indicator"></span>
+                    </label>';
+                }
+                return $button;
+            })
+            ->rawColumns(['icon', 'action', 'status'])
             ->setRowId('id');
     }
 
@@ -62,15 +87,15 @@ class CategoryDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            Column::make('id'),
+            Column::make('icon'),
+            Column::make('name'),
+            Column::make('status')->width(100),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(200)
                   ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
         ];
     }
 
